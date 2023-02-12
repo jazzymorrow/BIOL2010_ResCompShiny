e9ef46a64856ae74f6360e4e4121cadce47c4a76
unknown <jazzy.morrow23@gmail.com>
2023-02-13 09:39:40 +1000

    Resizing plot details

--- BIOL2010_ResComp/app.R
+++ BIOL2010_ResComp/app.R
@@ -8,6 +8,7 @@
 #devtools::install_github("andrewletten/rescomp")
 library(shiny)
 library(rescomp)
+library(ggplot2)
 
 #--------------------------------------------------
           # Define UI for application 
@@ -60,7 +61,31 @@ ui <- fluidPage(
                              value = 0.2)),
              mainPanel(
                plotOutput("TwoConOneRes"))
-    ))
+        )),
+    # PANEL 3: 2 CONS 1 PULSED RES
+    tabPanel(title = "Two Consumers, One Pulsed Resource", fluid = TRUE,
+             # Sidebar with a slider inputs 
+             sidebarLayout(
+               sidebarPanel(
+                 sliderInput("mu1",
+                             "Sp1 Maximum growth rate:",
+                             min = 0.01,
+                             max = 2,
+                             value = 0.2),
+                 sliderInput("mu2",
+                             "Sp2 Maximum growth rate:",
+                             min = 0.01,
+                             max = 2,
+                             value = 0.1),
+                 sliderInput("pulsefreq",
+                             "Pulse frequency:",
+                             min = 2,
+                             max = 100,
+                             value = 50)),
+               # Specify rescomp graph to use 
+               mainPanel(
+                 plotOutput("TwoConOnePulRes"))
+        ))
   )
 )
 
@@ -81,7 +106,11 @@ server <- function(input, output) {
         resconc = input$resconc,
         totaltime = 300)
       m1 <- sim_rescomp(pars)
-      plot_rescomp(m1)
+      plot_rescomp(m1) + theme(text = element_text(size=22),
+                               axis.title = element_text(size = 18),
+                               axis.text = element_text(size = 15),
+                               legend.text = element_text(size = 18),
+                               legend.position = "bottom")
     })
     
     ## Two consumers, one resource 
@@ -99,7 +128,38 @@ server <- function(input, output) {
         resconc = input$resconc,
         totaltime = 500)
       m1 <- sim_rescomp(pars)
-      plot_rescomp(m1)
+      plot_rescomp(m1) + theme(text = element_text(size=22),
+                               axis.title = element_text(size = 18),
+                               axis.text = element_text(size = 15),
+                               legend.text = element_text(size = 18),
+                               legend.position = "bottom")
+    })
+    
+    ## Two consumers, one pulsed resource 
+    output$TwoConOnePulRes <- renderPlot({
+      pars <- spec_rescomp(
+        spnum = 2, 
+        resnum = 1,
+        funcresp = "type2",
+        mumatrix = matrix(c(input$mu1,input$mu2), 
+                          nrow = 2, 
+                          ncol = 1,
+                          byrow = TRUE),
+        kmatrix = matrix(c(2, 0.015), 
+                         nrow = 2, 
+                         ncol = 1, 
+                         byrow = TRUE),  
+        resspeed = 0, # set to zero for no additional resource supply 
+        resconc = 0.2,
+        respulse = 0.3,
+        pulsefreq = input$pulsefreq # resource pulse size
+      )
+      m1 <- sim_rescomp(pars)
+      plot_rescomp(m1) + theme(text = element_text(size=22),
+                               axis.title = element_text(size = 18),
+                               axis.text = element_text(size = 15),
+                               legend.text = element_text(size = 18),
+                               legend.position = "bottom")
     })
      
 }
--- RescompTest.R
+++ RescompTest.R
@@ -19,7 +19,7 @@ pars <- spec_rescomp(
 m1 <- sim_rescomp(pars)
 plot_rescomp(m1)
 
-
+# Two consumers, one continuously supplied resource 
 pars <- spec_rescomp(
   spnum = 2, 
   resnum = 1,
@@ -34,3 +34,30 @@ pars <- spec_rescomp(
 
 m1 <- sim_rescomp(pars)
 plot_rescomp(m1)  
+
+
+# Two consumers, one pulsed resource 
+pars <- spec_rescomp(
+  spnum = 2, 
+  resnum = 1,
+  funcresp = "type2",
+  mumatrix = matrix(c(0.7,0.05), 
+                    nrow = 2, 
+                    ncol = 1,
+                    byrow = TRUE),
+  kmatrix = matrix(c(2, 0.015), 
+                   nrow = 2, 
+                   ncol = 1, 
+                   byrow = TRUE),  
+  resspeed = 0, # set to zero for no additional resource supply 
+  resconc = 0.2,
+  respulse = 0.3,
+  pulsefreq = 100, # resource pulse size
+  totaltime = 500
+)
+m1 <- sim_rescomp(pars)
+plot_rescomp(m1) + theme(text=element_text(size=20),
+                         axis.title = element_text(size = 10),
+                         axis.text = element_text(size = 10),
+                         legend.text = element_text(size = 10),
+                         legend.position = "bottom")
