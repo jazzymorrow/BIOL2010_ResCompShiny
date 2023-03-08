939b57f42427426caecf95f07f1abd6b60313bb2
unknown <jazzy.morrow23@gmail.com>
2023-03-08 15:23:54 +1000

    Fix initial parameter vals & add half saturation

--- BIOL2010_ResComp/app.R
+++ BIOL2010_ResComp/app.R
@@ -1,27 +1,33 @@
-#
+# Initial ----
+
 # Resource Competition Shiny App for BIOL2010
 # Created: 8/02/2023
 # Created by: Jasmine Fowler-Morrow
-#
-#    
-#
+
+# Use the navigation bar to navigate this script easily :)
+
 #devtools::install_github("andrewletten/rescomp")
 library(shiny)
 library(rescomp)
 library(ggplot2)
 
-#-----------------------------------------------------------
-                # Define UI for application 
-#-----------------------------------------------------------
+
+# UI for application ----
+
 ui <- navbarPage("Modelling Resource Competition",
     
-    ### PANEL 1: 1 CONS 1 RES
+    ## PANEL 1: 1 CONS 1 RES ----
     tabPanel(title = "One Consumer, One Resource", fluid = TRUE,
              # Sidebar with a slider inputs 
              sidebarLayout(
                sidebarPanel(width = 3,
                  sliderInput("mu",
                              "Maximum growth rate:",
+                             min = 0.01,
+                             max = 1,
+                             value = 0.2),
+                 sliderInput("halfsat", 
+                             "Half saturation:",
                              min = 0.01,
                              max = 1,
                              value = 0.2),
@@ -34,9 +40,10 @@ ui <- navbarPage("Modelling Resource Competition",
                # figures to include on this panel 
                mainPanel(h3("Functional response plots"),
                  fluidRow(column(12,align = "center",
-                                         plotOutput("FuncResp1", 
-                                                    width = "70%", 
-                                                    height = "300px"))),
+                                         plotOutput(
+                                           "FuncResp1",
+                                           width = "70%",
+                                           height = "300px"))),
                  h3("Population and resources through time"),
                  fluidRow(column(12,align = "center",
                                  plotOutput("OneConOneRes", 
@@ -45,24 +52,34 @@ ui <- navbarPage("Modelling Resource Competition",
              )),
     
     
-    ### 2 CONS 1 RES
+    ## PANEL 2: 2 CONS 1 RES ----
     # navbar menu to give drop-down options for cts or pulsed resourse 
     navbarMenu("Two Consumers, One Resource",
-    ### PANEL 2A: 2 CONS 1 RES CTS
+    ### PANEL 2A: 2 CONS 1 RES CTS ----
     tabPanel(title = "Continuously Supplied Resource", fluid = TRUE,
              # Sidebar with a slider inputs 
              sidebarLayout(
                sidebarPanel(width = 3,
-                 sliderInput("mu21", #mu for panel 2, species 1
-                             "Sp1 Maximum growth rate:",
+                 sliderInput("mu21", #mu for panel 2, sp 1
+                             "Sp1 maximum growth rate:",
+                             min = 0.01,
+                             max = 0.5,
+                             value = 0.3),
+                 sliderInput("halfsat21", #half sat for panel 2, sp 1
+                             "Sp1 half saturation:",
                              min = 0.01,
                              max = 1,
-                             value = 0.05),
-                 sliderInput("mu22",
-                             "Sp2 Maximum growth rate:",
+                             value = 0.45),
+                 sliderInput("mu22", #mu for panel 2, sp 2
+                             "Sp2 maximum growth rate:",
+                             min = 0.01,
+                             max = 0.5,
+                             value = 0.1),
+                 sliderInput("halfsat22", #half sat for panel 2, sp 2
+                             "Sp2 half saturation:",
                              min = 0.01,
                              max = 1,
-                             value = 0.2),
+                             value = 0.06),
                  sliderInput("resconc2",
                              "Resource concentration:",
                              min = 0.1,
@@ -71,81 +88,95 @@ ui <- navbarPage("Modelling Resource Competition",
                
                # figures to include on this panel
              mainPanel(h3("Functional response plots"),
-                       fluidRow(column(12,align = "center",
-                                       plotOutput("FuncResp2", 
-                                                  width = "70%", 
-                                                  height = "300px"))),
+                       fluidRow(
+                         column(12,align = "center",
+                                plotOutput("FuncResp2", 
+                                          width = "70%", 
+                                          height = "300px"))),
                h3("Population and resources through time"),
-               fluidRow(column(12,align = "center",
-                               plotOutput("TwoConOneRes", 
-                                          width = "90%", 
-                                          height = "300px"))))
+               fluidRow(
+                 column(12,align = "center",
+                        plotOutput("TwoConOneRes",
+                                   width = "90%", 
+                                   height = "300px"))))
         )),
     
-    ### PANEL 2B: 2 CONS 1 PULSED RES
+    ### PANEL 2B: 2 CONS 1 PULSED RES ----
     tabPanel(title = "Pulsed Resource", fluid = TRUE,
              # Sidebar with a slider inputs 
              sidebarLayout(
                sidebarPanel(width = 3,
-                 sliderInput("mu31", #mu for panel 3, species 1
-                             "Sp1 Maximum growth rate:",
+                 sliderInput("mu31", #mu for panel 3, sp 1
+                             "Sp1 maximum growth rate:",
+                             min = 0.01,
+                             max = 0.5,
+                             value = 0.3),
+                 sliderInput("halfsat31", #half sat for panel 3, sp 1
+                             "Sp1 half saturation:",
                              min = 0.01,
                              max = 1,
-                             value = 0.2),
+                             value = 0.45),
                  sliderInput("mu32",
-                             "Sp2 Maximum growth rate:",
+                             "Sp2 maximum growth rate:",
                              min = 0.01,
-                             max = 1,
+                             max = 0.5,
                              value = 0.1),
+                 sliderInput("halfsat32", #half sat for panel 3, sp 2
+                             "Sp2 half saturation:",
+                             min = 0.01,
+                             max = 0.5,
+                             value = 0.06),
                  sliderInput("pulsefreq",
-                             "Pulse frequency:",
-                             min = 2,
-                             max = 100,
+                             "Pulse interval size:",
+                             min = 50,
+                             max = 150,
                              value = 50)),
                
                # figures to include on this panel 
                mainPanel(h3("Functional response plot"),
-                         fluidRow(column(12,align = "center",
-                                         plotOutput("FuncResp3", 
-                                                    width = "70%", 
-                                                    height = "300px"))),
+                         fluidRow(
+                           column(12,align = "center",
+                                  plotOutput("FuncResp3", 
+                                            width = "70%",
+                                            height = "300px"))),
                          h3("Population and resources through time"),
-                         fluidRow(column(12,align = "center",
-                                         plotOutput("TwoConOnePulRes", 
-                                                    width = "90%", 
-                                                    height = "300px"))))
+                         fluidRow(
+                           column(12,align = "center",
+                                  plotOutput("TwoConOnePulRes",
+                                             width = "90%",
+                                             height = "300px"))))
         ))),
     
     
-    ### PANEL 4: 2 CONS 2 RES
+    ## PANEL 3: 2 CONS 2 RES ----
     tabPanel(title = "Two Consumers, Two Resources", 
              # Sidebar with a slider inputs 
              sidebarLayout(
                sidebarPanel(width = 3,
-                 sliderInput("mu41A",
+                 sliderInput("mu41A",# mu for panel 4, sp 1, res A
                              "Maximum growth rate for 
                              species 1 on resource A:",
                              min = 0.01,
-                             max = 1,
-                             value = 0.05),
-                 sliderInput("mu41B",
+                             max = 0.5,
+                             value = 0.12),
+                 sliderInput("mu41B",# mu for panel 4, sp 1, res B
                              "Maximum growth rate for 
                              species 1 on resource B:",
                              min = 0.01,
-                             max = 1,
-                             value = 0.05),
-                 sliderInput("mu42A",
+                             max = 0.5,
+                             value = 0.12),
+                 sliderInput("mu42A",# mu for panel 4, sp 2, res A
                              "Maximum growth rate for 
                              species 2 on resource A:",
                              min = 0.01,
-                             max = 1,
+                             max = 0.5,
                              value = 0.2),
-                 sliderInput("mu42B",
+                 sliderInput("mu42B",# mu for panel 4, sp 2, res B
                              "Maximum growth rate for 
                              species 2 on resource B:",
                              min = 0.01,
-                             max = 1,
-                             value = 0.05),
+                             max = 0.5,
+                             value = 0.2),
                  sliderInput("resconc4",
                              "Resource concentrations:",
                              min = 0.2,
@@ -155,9 +186,10 @@ ui <- navbarPage("Modelling Resource Competition",
                
                # figures to include on this panel
              mainPanel(h3("Functional response plots"),
-                       h5("This plot is used to identify R*. Use this 
-                          plot to determine the outcome: will species coexist,
-                          or will one species exclude the other?"),
+                       h5("This plot is used to identify R*. 
+                          Use this plot to determine the outcome: 
+                          will species coexist,or will one 
+                          species exclude the other?"),
                fluidRow(column(12,align = "center",
                         plotOutput("FuncResp4", 
                                       width = "80%", 
@@ -165,27 +197,31 @@ ui <- navbarPage("Modelling Resource Competition",
                h3("Population and resources through time"),
                fluidRow(column(12,align = "center",
                                plotOutput("TwoConTwoRes")))
-             ))
-  ))
+      )))
+)
 
 
 
 
-#---------------------------------------------------------------------
-                       # Define server logic 
-#---------------------------------------------------------------------
+
+# SERVER LOGIC ----
+
 server <- function(input, output) {
   
-    ## One consumer, one resource
+  ## 1 CONS 1 RES ----
+  
+  # FUNCTIONAL RESPONSE PLOT
   output$FuncResp1 <- renderPlot({
     pars <- spec_rescomp(
       spnum = 1, 
       resnum = 1,
       funcresp = "type2",
       mumatrix = matrix(input$mu),
+      kmatrix = matrix(input$halfsat),
       resspeed = 0.03,
       resconc = input$resconc,
       totaltime = 300)
+    
     plot_funcresp(pars, maxx = 1) + 
       theme(text = element_text(size = 22),
             axis.title = element_text(size = 18),
@@ -193,6 +229,8 @@ server <- function(input, output) {
             legend.text = element_text(size = 18),
             legend.position = "bottom")
   })
+  
+    # PLOT THROUGH TIME
     output$OneConOneRes <- renderPlot({
       #simulate based on input params from UI
       pars <- spec_rescomp(
@@ -200,6 +238,7 @@ server <- function(input, output) {
         resnum = 1,
         funcresp = "type2",
         mumatrix = matrix(input$mu),
+        kmatrix = matrix(input$halfsat),
         resspeed = 0.03,
         resconc = input$resconc,
         totaltime = 300)
@@ -213,9 +252,12 @@ server <- function(input, output) {
                                legend.text = element_text(size = 18),
                                legend.position = "bottom")
     })
-#################################################
-    ## Two consumers, one resource 
+    
+
+    ## 2 CONS 1 CTS RES ----
+    # FUNCTIONAL RESPONSE PLOT
     output$FuncResp2 <- renderPlot({
+      # specify rescomp parameters 
       pars <- spec_rescomp(
         spnum = 2, 
         resnum = 1,
@@ -223,10 +265,16 @@ server <- function(input, output) {
         mumatrix = matrix(c(input$mu21,input$mu22), 
                           nrow = 2, 
                           ncol = 1,
-                          byrow = TRUE),  
+                          byrow = TRUE), 
+        kmatrix = matrix(c(input$halfsat21, input$halfsat22),
+                         nrow = 2,
+                         ncol = 1,
+                         byrow = TRUE),
         resspeed = 0.03, 
         resconc = input$resconc2,
         totaltime = 500)
+      
+      # functional response plot 
       plot_funcresp(pars, maxx = 1) + 
         theme(text = element_text(size = 22),
               axis.title = element_text(size = 18),
@@ -234,16 +282,24 @@ server <- function(input, output) {
               legend.text = element_text(size = 18),
               legend.position = "bottom")
     })
+    
+    #PLOT THROUGH TIME
     output$TwoConOneRes <- renderPlot({
       #simulate based on input params from UI
       pars <- spec_rescomp(
         spnum = 2, 
         resnum = 1,
         funcresp = "type2",
-        mumatrix = matrix(c(input$mu21,input$mu22), 
+        mumatrix = matrix(c(input$mu21,
+                            input$mu22), 
                           nrow = 2, 
                           ncol = 1,
                           byrow = TRUE),  
+        kmatrix = matrix(c(input$halfsat21, 
+                           input$halfsat22), 
+                         nrow = 2, 
+                         ncol = 1, 
+                         byrow = TRUE), 
         resspeed = 0.03, 
         resconc = input$resconc2,
         totaltime = 500)
@@ -251,14 +307,18 @@ server <- function(input, output) {
       m1 <- sim_rescomp(pars)
       
       # plot results through time 
-      plot_rescomp(m1) + theme(text = element_text(size=22),
-                               axis.title = element_text(size = 18),
-                               axis.text = element_text(size = 15),
-                               legend.text = element_text(size = 18),
-                               legend.position = "bottom")
+      plot_rescomp(m1) + 
+        theme(text = element_text(size=22),
+              axis.title = element_text(size = 18),
+              axis.text = element_text(size = 15),
+              legend.text = element_text(size = 18),
+              legend.position = "bottom")
     })
-#################################################    
-    ## Two consumers, one pulsed resource 
+    
+    
+   
+    ## 2 CONS 1 PULSED RES ----
+    # FUNCTIONAL RESPONSE PLOT
     output$FuncResp3 <- renderPlot({
       pars <- spec_rescomp(
         spnum = 2, 
@@ -268,7 +328,7 @@ server <- function(input, output) {
                           nrow = 2, 
                           ncol = 1,
                           byrow = TRUE),
-        kmatrix = matrix(c(2, 2), 
+        kmatrix = matrix(c(input$halfsat31, input$halfsat32), 
                          nrow = 2, 
                          ncol = 1, 
                          byrow = TRUE),  
@@ -285,7 +345,8 @@ server <- function(input, output) {
               legend.text = element_text(size = 18),
               legend.position = "bottom")
     })
-    ## Two consumers, one pulsed resource 
+
+    #PLOT THROUGH TIME
     output$TwoConOnePulRes <- renderPlot({
       pars <- spec_rescomp(
         spnum = 2, 
@@ -295,7 +356,7 @@ server <- function(input, output) {
                           nrow = 2, 
                           ncol = 1,
                           byrow = TRUE),
-        kmatrix = matrix(c(2, 2), 
+        kmatrix = matrix(c(input$halfsat31, input$halfsat32), 
                          nrow = 2, 
                          ncol = 1, 
                          byrow = TRUE),  
@@ -308,14 +369,17 @@ server <- function(input, output) {
       m1 <- sim_rescomp(pars)
       
       # plot outcome through time 
-      plot_rescomp(m1) + theme(text = element_text(size=22),
-                               axis.title = element_text(size = 18),
-                               axis.text = element_text(size = 15),
-                               legend.text = element_text(size = 18),
-                               legend.position = "bottom")
+      plot_rescomp(m1) + 
+        theme(text = element_text(size=22),
+              axis.title = element_text(size = 18),
+              axis.text = element_text(size = 15),
+              legend.text = element_text(size = 18),
+              legend.position = "bottom")
     })
-#################################################    
-    ## Two consumers, two resources
+
+       
+    ## 2 CONS 2 RES ----
+    #FUNCTIONAL RESPONSE PLOT
     output$FuncResp4 <- renderPlot({
       pars <- spec_rescomp(
         spnum = 2, 
@@ -327,10 +391,13 @@ server <- function(input, output) {
                           ncol = 2,
                           byrow = TRUE),
         resspeed = 0.03,
-        resconc = input$resconc4, #both resources have same concentration??
+        resconc = input$resconc4, 
+        #both resources have same concentration??
         mort = 0.03,
         essential = FALSE,
         totaltime = 500)
+      
+      # functional response plot 
       plot_funcresp(pars, maxx = 1) + 
         theme(text = element_text(size = 22),
               axis.title = element_text(size = 18),
@@ -339,7 +406,7 @@ server <- function(input, output) {
               legend.position = "bottom")
     })
     
-    
+    #PLOT THROUGH TIME
     output$TwoConTwoRes <- renderPlot({
     pars <- spec_rescomp(
       spnum = 2, 
@@ -351,7 +418,7 @@ server <- function(input, output) {
                         ncol = 2,
                         byrow = TRUE),
       resspeed = 0.03,
-      resconc = input$resconc4, #both resources have same concentration??
+      resconc = input$resconc4, 
       mort = 0.03,
       essential = FALSE,
       totaltime = 500)
@@ -368,5 +435,5 @@ server <- function(input, output) {
      
 }
 
-# Run the application 
+# Run the application ----
 shinyApp(ui = ui, server = server)
